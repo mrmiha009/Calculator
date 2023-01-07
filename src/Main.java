cd7d6b058616522cb2d87d87ec7256a9870c8279
mrmiha009 <pfc.cska.rfs@ya.ru>
2023-01-07 14:22:00 +0300

    коммент

--- src/Main.java
+++ src/Main.java
@@ -22,8 +22,8 @@ class Main {
                          "LI","LII","LIII","LIV","LV","LVI","LVII","LVIII","LIX","LX",//51-60
                          "LXI","LXII","LXIII","LXIV","LXV","LXVI","LXVII","LXVIII","LXIX","LXX",//61-70
                          "LXXI","LXXII","LXXIII","LXXIV","LXXV","LXXVI","LXXVII","LXXVIII","LXXIX","LXXX",//71-80
-                         "LXXXI","LXXXII","LXXXIII","LXXXIV","LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC",//71-90
-                         "XCI","XCII","XCIII","XCIV","XCV","XCVI","XCVII","XCVIII","XCIX","C"};//31-100
+                         "LXXXI","LXXXII","LXXXIII","LXXXIV","LXXXV","LXXXVI","LXXXVII","LXXXVIII","LXXXIX","XC",//81-90
+                         "XCI","XCII","XCIII","XCIV","XCV","XCVI","XCVII","XCVIII","XCIX","C"};//91-100
         String[] arabic = {"1","2","3","4","5","6","7","8","9","10"}; // перечисление арабских цифр
         for (int x = 0; x <= 9 ; x++) // римские
             if (rome[x].equals(a)){
